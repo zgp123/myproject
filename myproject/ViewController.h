f7529c29999b18a3c5e2b63a7c9231f051c5b957
张国平 <zhangguoping@zhangguopingdeMacBook-Pro.local>
2015-05-30 11:30:28 +0800

    键入

---
+++ myproject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  myproject
+//
+//  Created by 张国平 on 15/5/30.
+//  Copyright (c) 2015年 张国平. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ myproject/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  myproject
+//
+//  Created by 张国平 on 15/5/30.
+//  Copyright (c) 2015年 张国平. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
